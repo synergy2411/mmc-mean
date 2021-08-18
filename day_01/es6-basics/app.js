1765ebc430631bf9dc414d1b80453926e89d981c
Sumit Khandelwal <sumitgniit@gmail.com>
2021-08-18 10:33:01 +0530

    initial commit

---
+++ day_01/es6-basics/app.js
@@ -0,0 +1,87 @@
+// ES6 Features
+
+// Arrow Function : () => 
+
+// var numbers = [1,2,3,4,5];
+// // ES5 / Vanilla JS
+// var doubleValue = numbers.map(function(val){
+//     return val * 2;
+// })
+
+// // Arrow Function
+// var tripleValue = numbers.map((val, index) =>{ 
+//     console.log("Index : ", index)
+//     return val * 3
+// })
+
+// console.log(doubleValue);
+// console.log(tripleValue);
+
+
+// Destructuring : Array | Objects
+
+// var arr = [3,4,5]
+// var newArr = [1,2,...arr, 6,7,8];
+
+// console.log(newArr);    
+
+// 1,2,3,45,6,7,8
+// 1,2,[3,4,5],6,7,8
+
+
+// var friends = ["foo","Bar","bam","baz"]
+
+// var [arr1, , arr3, arr4] = friends;
+
+// console.log(arr3);          // ?
+
+// var user = {
+//     firstName : "Foo",
+//     lastName : "BAr",
+//     age : 32
+// }
+
+// var { lastName, age, firstName } = user;
+// console.log(lastName, age)
+
+
+// var user = {
+//     email : "test@test.com",
+//     age : 32,
+//     address : {
+//         city : "Bengaluru",
+//         street : "201 Main Road, Marathahalli"
+//     },
+//     friends : ["f1", "f2", "f3"]
+// }
+
+// var { email, age, address : { city, street } } = user;
+
+// console.log(email, city, street, f2)
+
+// var user = {
+//     email : "test@test.com",
+//     age : 32,
+//     address : {
+//         city : "Bengaluru",
+//         street : "201 Main Roa, Marathahalli"
+//     },
+//     friends : ["f1", "f2", "f3"]
+// }
+
+// var {email, age, address : {city,street}, friends : [f1,f2,f3]} = user;
+// console.log(city,street);
+
+
+// Template String Syntax - "" | '' | ``
+//  - Embed variable within string
+// - Multiline String
+
+var username = "Foo Bar";
+
+var greet = `Hello from ${username},
+How are you?
+`;
+
+console.log(greet);
+
