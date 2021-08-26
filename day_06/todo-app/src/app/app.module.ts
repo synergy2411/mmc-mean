073a1e2d170911b1dd21da50e629ddf60f2fe326
Sumit Khandelwal <sumitgniit@gmail.com>
2021-08-26 09:47:22 +0530

    2 way binding added

--- day_06/todo-app/src/app/app.module.ts
+++ day_06/todo-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
@@ -11,7 +12,8 @@ import { TodoComponent } from './components/todo/todo.component';
     TodoComponent
   ],
   imports: [          // Modules : built-in | Custom
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],      // Services
   bootstrap: [AppComponent]
--- day_06/todo-app/src/app/components/todo/todo.component.ts
+++ day_06/todo-app/src/app/components/todo/todo.component.ts
@@ -8,4 +8,27 @@ import { Component, ViewEncapsulation } from '@angular/core'
 })
 export class TodoComponent{
 
+  country : string = 'india';
+
+  user = {
+    firstName : "Bill",
+    lastName : "Gates",
+    dob : new Date("Dec 21, 1965"),
+    income : 50000,
+    isWorking : true,
+    company : "Microsoft",
+    votes : 120,
+    image : "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f4ebe0c87612dab4f12a597%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D292%26cropX2%3D3684%26cropY1%3D592%26cropY2%3D3987"
+  }
+
+  onMoreInfo(usr : any): any{
+    alert(`Mr. ${usr.lastName} is working with ${usr.company}!`)
+  }
+
+  todoItem = {
+    id : 1,
+    label : "insurance",
+    status : false
+  }
+
 }
